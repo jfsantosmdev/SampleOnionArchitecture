110132fad554875f1a4415963c02bd3964c31ade
Juan Fernando Santos <jfernando.santosm@gmail.com>
2022-06-27 21:27:03 -0600

    Created first domain entity

---
+++ Application/Features/Clients/Commands/CreateClientCommand.cs
@@ -0,0 +1,16 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+
+namespace Application.Features.Clients.Commands
+{
+    public class CreateClientCommand : IRequest<Response<int>>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+        private int _age;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BrithDate { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Age
+        {
+            get { 
+                if(_age <= 0)
+                {
+                    _age = new DateTime(DateTime.Now.Subtract(this.BrithDate).Ticks).Year - 1;
+                }
+                return _age;
+            }
+        }
+    }
+}
