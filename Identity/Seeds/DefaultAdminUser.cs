b1819ff87ef250c2baa97386f928ab7ae038708e
Juan Fernando Santos <jfernando.santosm@gmail.com>
2022-07-19 12:41:21 -0600

    Configured default roles and users

---
+++ Identity/Seeds/DefaultAdminUser.cs
@@ -0,0 +1,34 @@
+﻿using Application.Enums;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Seeds
+{
+    public static class DefaultAdminUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Users
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "Admin",
+                Email = "admin@test.com",
+                FirstName = "Juan Fernando",
+                LastName = "Santos",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Admin$123");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Basic.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Identity/Seeds/DefaultBasicUser.cs
@@ -0,0 +1,33 @@
+﻿using Application.Enums;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Seeds
+{
+    public static class DefaultBasicUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Users
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "Admin",
+                Email = "basic@test.com",
+                FirstName = "Juan Fernando",
+                LastName = "Santos",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Basic$123");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Basic.ToString());
+                }
+            }
+        }
+    }
+}
