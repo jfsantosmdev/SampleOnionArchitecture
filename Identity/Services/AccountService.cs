08e9768f9e2416e3fedbe46e0c447a9794d6e330
Juan Fernando Santos <jfernando.santosm@gmail.com>
2022-07-19 22:58:09 -0600

    Created Account Service

---
+++ Identity/Services/AccountService.cs
@@ -0,0 +1,96 @@
+﻿using Application.DTOs.Users;
+using Application.Enums;
+using Application.Exceptions;
+using Application.Interfaces;
+using Application.Wrappers;
+using Domain.Settings;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JWTSettings _jwtSettings;
+        private readonly IDateTimeService _dateTimeService;
+
+        public AccountService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager, JWTSettings jwtSettings, IDateTimeService dateTimeService)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwtSettings;
+            _dateTimeService = dateTimeService;
+        }
+
+        public async Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user != null)
+            {
+                throw new ApiException($"Email {request.Email} doesn't exists");
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+            if (!result.Succeeded)
+            {
+                throw new ApiException($"Invalid credentials");
+            }
+
+            JwtSecurityToken token = await GenerateJWToken(user);
+            return null;
+        }
+
+        public async Task<Response<string>> RegisterAsync(RegisterRequest request, string origin)
+        {
+            var userNameExists = await _userManager.FindByNameAsync(request.UserName);
+            if(userNameExists != null)
+            {
+                throw new ApiException($"Username {request.UserName} already exists");
+            };
+
+            var user = new ApplicationUser
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                Email = request.Email,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+
+            var emailExists = await _userManager.FindByEmailAsync(request.Email);
+            if(emailExists != null)
+            {
+                throw new ApiException($"Email {request.Email} already exists");
+            }
+            else
+            {
+                var result = await _userManager.CreateAsync(user, request.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, Roles.Basic.ToString());
+                    return new Response<string>(user.Id, message: $"Successfully registered user");
+                }
+                else
+                {
+                    throw new ApiException($"{result.Errors}");
+                }
+            }
+        }
+
+        private async Task<JwtSecurityToken> GenerateJWToken(ApplicationUser user)
+        {
+            return null;
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,35 @@
+using Identity.Models;
+using Identity.Seeds;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    await DefaultRoles.SeedAsync(userManager, roleManager);
+                    await DefaultAdminUser.SeedAsync(userManager, roleManager);
+                    await DefaultBasicUser.SeedAsync(userManager, roleManager);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
